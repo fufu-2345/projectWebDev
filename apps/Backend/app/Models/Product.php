9de9ec6211ed8d180548d47a6058fdd32c9db09e
fufu-2345 <mr.kittisak.ploysumrit@gmail.com>
2025-08-27 20:21:45 +0700

    add some

---
+++ apps/Backend/app/Models/Product.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable =[
+        "a1","a2","a3"
+    ];
+}
---
+++ apps/Backend/database/migrations/2025_08_25_151829_create_product_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('Product', function (Blueprint $table) {
+            $table->id();
+            $table->double('a1', 8, 2);
+            $table->integer('a2');
+            $table->string('a3', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
